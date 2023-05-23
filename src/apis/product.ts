855d41f95328972167e2ad9b6ea5e0eeac7407bc
kingsley-sunny <ezeobisunny51@gmail.com>
2023-05-23 16:08:11 +0100

    [corrections]: fixed the validation of creating a product to be showing detailed messages

--- src/apis/product.ts
+++ src/apis/product.ts
@@ -9,7 +9,6 @@ import {
   getProducts,
 } from "src/controllers";
 import { isAuth } from "src/middlewares";
-import { validate } from "src/utils";
 import validator from "validator";
 
 const MAX_IMAGE_SIZE = 2_000_000_000;
@@ -18,45 +17,50 @@ const validateProductReqBody = (
   req: Request<any, any, ICreateProductBody>
 ): { success: boolean; msg: string } => {
   const { description, location, model, name, type, price } = req.body;
-  try {
-    const isDescriptionValid = validate(
-      validator.isLength(description, { min: 5 }),
-      "Description Must be Greater 5 characters"
-    );
-    const isLocationValid = validate(
-      validator.isLength(location, { min: 1 }),
-      "Please Fill in your location"
-    );
-    const isModelValid = validate(
-      validator.isLength(model, { min: 1 }),
-      "Product Model is required"
-    );
-    const isNameValid = validate(validator.isLength(name, { min: 1 }), "Product Name is required");
-    const isPriceValid = validate(price >= 0, "Product price cannot be zero");
-    const isTypeValid = validate(
-      validator.equals(type, "new") || validator.equals(type, "used"),
-      "Product Type should be used or new"
-    );
 
-    const isAllFieldsValid =
-      isDescriptionValid &&
-      isLocationValid &&
-      isModelValid &&
-      isNameValid &&
-      isTypeValid &&
-      isPriceValid;
+  const errorMsg = { success: false, msg: "NO valid credentials" };
 
-    if (isAllFieldsValid) {
-      return {
-        success: true,
-        msg: "success",
-      };
-    }
+  const isDescriptionValid = validator.isLength(description, { min: 5 });
 
-    return { success: false, msg: "NO valid credentials" };
-  } catch (error: any) {
-    return { success: false, msg: error.message };
+  const isLocationValid = validator.isLength(location, { min: 1 });
+
+  const isModelValid = validator.isLength(model, { min: 1 });
+
+  const isNameValid = validator.isLength(name, { min: 1 });
+  const isPriceValid = price > 0;
+  const isTypeValid = validator.equals(type, "new") || validator.equals(type, "used");
+
+  if (!isDescriptionValid) {
+    errorMsg.msg = "Description Must be Greater 5 characters";
+    return errorMsg;
   }
+
+  if (!isLocationValid) {
+    errorMsg.msg = "Please Fill in your location";
+    return errorMsg;
+  }
+
+  if (!isModelValid) {
+    errorMsg.msg = "Product Model is required";
+    return errorMsg;
+  }
+
+  if (!isNameValid) {
+    errorMsg.msg = "Product Name is required";
+    return errorMsg;
+  }
+
+  if (!isPriceValid) {
+    errorMsg.msg = "Product price cannot be zero";
+    return errorMsg;
+  }
+
+  if (!isTypeValid) {
+    errorMsg.msg = "Product Type should be used or new";
+    return errorMsg;
+  }
+
+  return { success: true, msg: "success" };
 };
 
 const storage = multer.diskStorage({
