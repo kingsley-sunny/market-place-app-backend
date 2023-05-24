482c775cda8be83e376bcb6bb680f59e8f5ea31e
kingsley-sunny <ezeobisunny51@gmail.com>
2023-05-24 21:42:07 +0100

    Added endpoint to get all items from the cart and the remove item from the cart

--- src/apis/cart.ts
+++ src/apis/cart.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { addToCart } from "src/controllers/cart/cart";
+import { addToCart, getCartItems, removeItemFromCart } from "src/controllers/cart/cart";
 
 const router = Router();
 
 router.post("/add", addToCart);
+
+router.delete("/remove/:productId", removeItemFromCart);
+
+router.get("/", getCartItems);
 
 export const cartRoutes = router;
--- src/controllers/cart/cart.ts
+++ src/controllers/cart/cart.ts
@@ -25,8 +25,6 @@ export const addToCart = async (
       where: { productId: productId, cartId: cart.dataValues.id },
     });
 
-    console.log(cartItem);
-
     if (!cartItem) {
       // if it is NOT in the cart we will add the product and the make the quantity to 1
       const newCartItem = CartItems.build({
@@ -50,4 +48,63 @@ export const addToCart = async (
     // console.log(error);
     next(error);
   }
+};
+
+export const removeItemFromCart = async (
+  req: Request<CartBody, any>,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const productId = req.params.productId;
+    const userId = req.userId;
+    const cart = await Cart.findOne({ where: { userId: userId } });
+
+    if (!cart) {
+      const err = createErrorObj("No Cart found");
+      err.status = 401;
+      throw err;
+    }
+
+    // we check if the productId is already in the cart.
+    const cartItem = await CartItems.findOne({
+      where: { productId: productId, cartId: cart.dataValues.id },
+    });
+
+    // If it is not in the cart, we will send back an error message that the product is not found
+    if (!cartItem) {
+      const err = createErrorObj("This Product is not in the cart");
+      err.status = 400;
+      throw err;
+    }
+
+    // If it is in the cart and the quantity is higher than 1, we will reduce it
+    if (cartItem.dataValues.quantity > 1) {
+      const newCartItem = { ...cartItem.dataValues };
+      newCartItem.quantity -= 1;
+      cartItem.update({ quantity: newCartItem.quantity });
+      return res.status(201).json(createSuccessObj({}, "SuccessFul", 201));
+    }
+
+    // but if it is lesser than 1, we will remove the entire item from the cart
+    if (cartItem.dataValues.quantity <= 1) {
+      cartItem.destroy();
+      return res
+        .status(201)
+        .json(createSuccessObj({}, "SuccessFul Removed Item from the cart", 201));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getCartItems = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  const userId = req.userId;
+  const cartItems = await CartItems.findAll({ where: { cartId: userId } });
+
+  res.status(200).json(createSuccessObj(cartItems));
 };
